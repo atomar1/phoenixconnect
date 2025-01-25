11e563a84a01a9919a342963fb1a62ee71fafdb4
Ivan Leung <ivanl8866@gmail.com>
2025-01-25 11:05:05 -0800

    Create Home component

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,21 @@
-import Navbar from './components/Navbar.jsx';
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import './App.css'
+import Home from './pages/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
 
 const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <BrowserRouter>
+      <div
+        className="flex flex-col m-0 p-4 max-w-full w-screen h-screen max-h-screen overflow-auto antialiased"
+      >
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
