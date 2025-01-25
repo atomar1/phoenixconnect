9771f1ccd02b24d891bccd6b85cae82cf6aa7d39
Kary Zheng <150742834+kz930@users.noreply.github.com>
2025-01-25 12:27:41 -0800

    Update __init__.py

--- flaskMongoDB/application/__init__.py
+++ flaskMongoDB/application/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import PyMongo
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "c2bd371f08b8a505f82b04819ffc133ff8eb3a7e"
-app.config["MONGO_URI"] = "mongodb+srv://phoenix:phoenix123@cluster0.0uadu.mongodb.net/todo_db?retryWrites=true&w=majority"
+app.config["MONGO_URI"] = "mongodb+srv://phoenix:phoenix123@cluster0.0uadu.mongodb.net/userinfo_db?retryWrites=true&w=majority"
 
 mongodb_client = PyMongo(app)
 db = mongodb_client.db
