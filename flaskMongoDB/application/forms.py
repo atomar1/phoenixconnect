a6c13e5acc49cd7651ecc627c5b51cd76a7a58f5
Kary Zheng <150742834+kz930@users.noreply.github.com>
2025-01-25 12:27:54 -0800

    Update forms.py

--- flaskMongoDB/application/forms.py
+++ flaskMongoDB/application/forms.py
@@ -1,10 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, TextAreaField, SelectField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, EqualTo, Length
 
-class TodoForm(FlaskForm):
-    name = StringField("Name", validators=[DataRequired()])
-    description = TextAreaField("Description", validators = [DataRequired()])
-    completed = SelectField("Completed", choices = [("False","False"), ("True", "True")],
-                            validators = [DataRequired()])
-    submit = SubmitField("Add Todo")
+
+class SignInForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired(), Length(min=4, max=20)])
+    password = PasswordField("Password", validators=[DataRequired(), Length(min=6)])
+    confirm_password = PasswordField("Confirm Password", validators=[
+        DataRequired(), EqualTo('password', message="Passwords must match")
+    ])
+    submit = SubmitField("Sign Up")
+
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    submit = SubmitField("Log In")
